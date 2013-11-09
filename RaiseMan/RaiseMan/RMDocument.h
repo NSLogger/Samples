f97bcd1b34efa610c4674f50ea5cfd9039e147dc
Arun Agrawal <arunagw@gmail.com>
2013-11-09 22:04:54 +0100

    RaiseMan intial

---
+++ RaiseMan/RaiseMan/RMDocument.h
@@ -0,0 +1,13 @@
+//
+//  RMDocument.h
+//  RaiseMan
+//
+//  Created by Arun Agrawal on 09/11/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RMDocument : NSDocument
+
+@end
---
+++ RaiseMan/RaiseMan/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RaiseMan
+//
+//  Created by Arun Agrawal on 09/11/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
